c7923443effef5b8529dd2162e9e7f359bfb86a8
shivani <shivanithakur687@gmail.com>
2018-07-27 13:52:20 +0530

    oka

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,7 +13,7 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 
-THEME = "/home/shivani/pelican-themes/notebook"
+THEME = "/home/shivani/pelican-themes/pelican-striped-html5up"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
