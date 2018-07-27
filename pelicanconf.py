c6e9132f13fe17082b9fc8569f3b565a8ca847c5
shivani <shivanithakur687@gmail.com>
2018-07-27 13:33:47 +0530

    pelican

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,7 +13,7 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 
-THEME = "/home/shivani/pelican-themes/bricks"
+THEME = "/home/shivani/pelican-themes/franticworld"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
